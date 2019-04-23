bf5bd7bb50aab461d53e5ec42ebc51038656fb7f
Dan Hogan <hogan.daniel@hotmail.com>
2019-04-23 16:31:54 -0700

    Created index.js file and linked to HTML page, also created variables and linked them to the HTML quiz elements

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const inputOne = document.getElementById('question1');
+const inputTwo = document.getElementById('question2');
+const inputThree = document.getElementById('question3');
+const inputFour = document.getElementById('question4');
+const inputFive = document.getElementById('question5');
+const button = document.getElementById('submit-answers');
+
+button.addEventListener('click', event => {
+    event.preventDefault();
+    
+});
