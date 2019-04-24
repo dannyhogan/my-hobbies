3011d1e6dfdc6354f99ec69a42121b2a61fadd3e
Dan Hogan <hogan.daniel@hotmail.com>
2019-04-23 19:00:09 -0700

    Changed two answers from integers to strings

--- test/tests.js
+++ test/tests.js
@@ -5,9 +5,9 @@ test('return a score of 5 if all answers are correct', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const answerOne = 'fortnite';
-    const answerTwo = 10;
+    const answerTwo = '10';
     const answerThree = 'mt hood';
-    const answerFour = 3;
+    const answerFour = '3';
     const answerFive = 'Nintendo64';
 
     const expectedResult = 5;
@@ -22,9 +22,9 @@ test('return a score of 0 if all answers are incorrect', function(assert) {
     //Arrange
     // Set up your parameters and expectations
     const answerOne = 'Donkey Kong';
-    const answerTwo = 12;
+    const answerTwo = '12';
     const answerThree = 'Mt Bachelor';
-    const answerFour = 1;
+    const answerFour = '1';
     const answerFive = 'Wii';
 
     const expectedResult = 0;
@@ -39,9 +39,9 @@ test('return a score of 3 if 2 of the 5 answers are incorrect', function(assert)
     //Arrange
     // Set up your parameters and expectations
     const answerOne = 'Pokemon';
-    const answerTwo = 10;
+    const answerTwo = '10';
     const answerThree = 'Mt Hood';
-    const answerFour = 5;
+    const answerFour = '5';
     const answerFive = 'Nintendo 64';
 
     const expectedResult = 3;
